bdd961d42cd6426f6d574982ea4cd763205b6fe8
Sabbir Ahmmed <ahmmedsabbirbd@gmail.com>
2023-01-23 14:28:55 +0600

    day 4 complete

---
+++ day04/endIf.php
@@ -0,0 +1,25 @@
+<?php
+
+$name = 'sabbir';
+
+if ('sabbir' == $name):
+    echo "refujir bor";
+else:
+    echo "not refujir bor";
+endif;
+
+?>
+
+Ki re 
+
+<?php
+if (2 == 2) : 
+    ?>
+        Doner matha
+        <?php
+else : 
+    ?>
+        Doner matha
+    <?php
+endif;
+?>
---
+++ day04/switch.php
@@ -0,0 +1,14 @@
+<?php
+$name = 'oishi';
+switch ($name) {
+    case 'sabbir':
+        echo "{$name} is good man";
+        break;
+    case 'oishi': 
+        echo "{$name} is bad man";
+        break;
+    case 'refuji': 
+            echo "{$name} is khati refuji";
+        break;
+    default: 
+}
